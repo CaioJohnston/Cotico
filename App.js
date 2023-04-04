4beadba84226c26a2af024e33196db040cbf69f3
Vinicius Rayol <102189417+Viinee1@users.noreply.github.com>
2023-04-04 00:09:08 -0300

    Add files via upload
    Cotico tela de login protótipo 1.0

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from './components/loginsenha';
+
+function App() {
+  return (
+    <div className="telalogin"> 
+        <Login></Login>       
+    </div>
+    
+  );
+}
+
+export default App;
